216880c279fde9e367d45c63e8b252e9b0b304fb
Zarman-oss <zarmanimran@gmail.com>
2023-07-22 04:46:19 +0500

    added array data

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
 
-function Header({ text }) {
+function Header({ text, bgColor, textColor }) {
   return (
     <header>
-      <div className="className">
+      <div className="container">
         <h1>{text}</h1>
       </div>
     </header>
@@ -12,10 +12,14 @@ function Header({ text }) {
 
 Header.defaultProps = {
   text: 'Feedback UI',
+  bgColor: 'rgba (0,0,0,0.4)',
+  textColor: '#ff6a95',
 };
 
 Header.prototypes = {
   text: PropTypes.string,
+  bgColor: PropTypes.string,
+  textColor: PropTypes.string,
 };
 
 export default Header;
